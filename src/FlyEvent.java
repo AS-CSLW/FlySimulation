45b64627385cb1943d1281d476934a7e4fb5f0a5
saka <asclepius1993@gmail.com>
2017-04-23 16:55:07 -0400

    add geometry related methods

--- src/FlyEvent.java
+++ src/FlyEvent.java
@@ -1,7 +1,7 @@
 
 public class FlyEvent extends Event{
 	public static final int changeDirection = 1;
-	public static final int KnockIntoWall = 2;
+	public static final int knockIntoWall = 2;
 	public static final int flyIntoTrap = 3;
 	
 	FlyEvent(double delay, EventHandler handler, int EventType) {
