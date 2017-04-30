4f50fd290ec8916d3a420b944f0a8fc03653c1d9
xiaoyangmeng <mxynapoleon@gmail.com>
2017-04-30 14:41:12 -0400

    construct a 4 segments contained room may have some conflict, need discuss

--- src/Room.java
+++ src/Room.java
@@ -1,28 +1,41 @@
 /// Authors: Xiaoyang MENG, Cong DU, Soowoo CHANG, Wuchang LI
+import javafx.geometry.Pos;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Room {
 	private List<Line> boundarys = null;
-	//information to help drawing. or automate generate fly in room randomly (combine with isInsideRoom method).
-	public double Xmax,Xmin,Ymax,Ymin;
-	
-	/*
-	 * Cong: assume the boundary of room is always described as polygon.
-	 * 		the boundarys should be a series of points in clockwise order or anticlockweise order.
-	 */
-	Room(List<Position> b) {
-		boundarys = null;
-		//also determine Xmax, Xmin, Ymax, Ymin for Fly to use.
+
+
+	private Position p1, p2, p3, p4;
+
+	// Xiaoyang: this is always a 4 segments polygon. not necessary to be a square.
+	public Room (Position Xmax, Position Xmin, Position Ymax, Position Ymin)
+	{
+		p1 = Xmax;
+		p2 = Xmin;
+		p3 = Ymax;
+		p4 = Ymin;
 	}
-	
-	public boolean checkAcross(Position begin,Position end) {
-		//Cong: this method check whether the imaginary path intersect with boundary (cause knock into boundary event)
+
+	// Xiaoyang: give the position of the corners of this polygon. 
+	public static void main(String args[]) {
+		Position Room[] = {new Position(0, 0), new Position(10, 0),
+				new Position(10, 10), new Position(0, 10)};
+	}
+
+	public  static boolean checkAcross(Position p,Position q, Position r) {
+		if (q.X <= Math.max(p.X, r.X) && q.X >= Math.min(p.X, r.X)
+				&& q.Y <= Math.max(p.Y, r.Y) && q.Y >= Math.min(p.Y, r.Y))
+		//Xiaoyang: this method check whether the imaginary path intersect with boundary (cause knock into boundary event)
 		return true;
 	}
 	
-	public Line returnAcrossWall(Position begin,Position end) {
+	public Line returnAcrossWall(Position p,Position r) {
 		//return the intersected wall
+		p1 = p;
+		p3 = r;
 		return new Line(new Position(0,0),new Position(1,1));
 	}
 	
