7b99e073fd8493e4d759ea23ba2653b2beb1cb35
saka <asclepius1993@gmail.com>
2017-04-29 21:03:35 -0400

    add trap

--- src/FlyEvent.java
+++ src/FlyEvent.java
@@ -1,8 +1,7 @@
 
 public class FlyEvent extends Event{
 	public static final int changeDirection = 1;
-	public static final int knockIntoWall = 2;
-	public static final int flyIntoTrap = 3;
+	public static final int flyIntoTrap = 2;
 	
 	FlyEvent(double delay, EventHandler handler, int EventType) {
 		super(delay,handler,EventType);
