934b8e8a844219910d21a68a87fe9a3bd744fa88
wuchang li <wli391@gatech.edu>
2017-04-29 21:08:42 -0400

    modified intersectpos func

--- src/Line.java
+++ src/Line.java
@@ -7,7 +7,12 @@ public class Line {
 	Line(Position pos1,Position pos2) {
 		p1 = pos1;
 		p2 = pos2;
-		coefficientOfSlope = (p1.Y-p2.Y)/(p1.X-p2.X);
+		if(p1.Y == p2.Y){
+			coefficientOfSlope = 0;
+		}else {
+				coefficientOfSlope = (p1.Y-p2.Y)/(p1.X-p2.X);
+
+		}
 	}
 	
 	static public boolean doInteresct(Line l1, Line l2) {
@@ -20,11 +25,22 @@ public class Line {
 		double m1 = l1.coefficientOfSlope;
 		//coefficientOfSlope2
 		double m2 = l2.coefficientOfSlope;
-
-		a.X = (l2.p1.Y - l1.p1.Y + m1 * l1.p1.X -m2 * l2.p1.X) / (m1 - m2);
-		a.Y = m1 * (a.X - l1.p1.X) + l1.p1.Y;
+		if(Double.isFinite(m1)) {
+			a.X = (l2.p1.Y - l1.p1.Y + m1 * l1.p1.X - m2 * l2.p1.X) / (m1 - m2);
+			a.Y = m1 * (a.X - l1.p1.X) + l1.p1.Y;
+		}else{
+			if(!(Double.isFinite(m1))){
+				a.X = l1.p1.X;
+				a.Y = m2 * (a.X - l2.p1.X) + l2.p1.Y;
+			}else{
+				a.X = l2.p1.X;
+				a.Y = m1 * (a.X - l1.p1.X) + l1.p1.Y;
+			}
+		}
 		return a;
 	}
+
+
 	
 	
 }
