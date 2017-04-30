b1cafe33f43b7a5eda22d6576391cd576adce5a6
xiaoyangmeng <mxynapoleon@gmail.com>
2017-04-30 18:04:55 -0400

    building the trap

--- src/Trap.java
+++ src/Trap.java
@@ -1,15 +1,30 @@
 
 public class Trap {
-	double length;
+	private int m_length;
+	private double m_xlength;
+	private double m_ylength;
+
+	public Position p1, p2;
+
+
+	private Line m_line;
+
+	private Room room;
+
+	public trap(Line line, Position postion, Position position, double length)
+	{
+		m_line = line;
+		m_length = length;
+	}
+	public Line getLine() {return m_line;}
+	public double getSize(){return m_length;}
+
 	Line entity;
 	Room room;
 	
-	Trap(Room r,double length) {
-		
-	}
-	
+
 	Trap(Room r,Position p1,Position p2) {
-		//a temporary constrctor method, just for trail.
+		//a temporary constructor method, just for trail.
 		//so Position p1 and p2 are not examed;
 		this.room = r;
 		this.entity = new Line(p1,p2);
