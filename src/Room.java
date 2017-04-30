32531cf39090907e247ab06803f044902f8cc4f1
wuchang li <wli391@gatech.edu>
2017-04-30 18:01:45 -0400

    finish room.class

--- src/Room.java
+++ src/Room.java
@@ -5,43 +5,73 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Room {
-	private List<Line> boundarys = null;
+	private List<Line> boundaries = null;
+	public double Xmax = 0,Xmin = 0,Ymax = 0,Ymin = 0;
+	Room(){
+		int i = 0;
+		for(Line boundary : boundaries){
+			Line tempLine1, tempLine2;
+			tempLine1 = boundaries.get(i);
+			tempLine2 = boundaries.get(i+1);
+			Position intersectPoint = Line.intersectPos(tempLine1, tempLine2);
+			if(intersectPoint.X > Xmax){
+					Xmax = intersectPoint.X;
+			}
+			if(intersectPoint.Y > Ymax){
+				Ymax = intersectPoint.Y;
+			}
+			if(intersectPoint.X < Xmin){
+				Xmin = intersectPoint.X;
+			}
+			if(intersectPoint.Y < Ymin){
+				Ymin = intersectPoint.Y;
+			}
 
-
-	private Position p1, p2, p3, p4;
-
+		}
+	}
+	// clockwise
 	// Xiaoyang: this is always a 4 segments polygon. not necessary to be a square.
-	public Room (Position Xmax, Position Xmin, Position Ymax, Position Ymin)
-	{
-		p1 = Xmax;
-		p2 = Xmin;
-		p3 = Ymax;
-		p4 = Ymin;
+
+	public boolean pointIsInTheRoom(Position a){
+		int count = 0;
+		Position virtualPoint = new Position((double)(Integer.MAX_VALUE), a.Y);
+		Line virtualLine = new Line(a,virtualPoint);
+		for(Line boundary : boundaries)
+		{
+			if(Line.doInteresct(virtualLine, boundary))
+				count++;
+		}
+
+		if(count%2==1)
+			return true;
+		else
+			return false;
 	}
 
-	
 	public boolean checkAcross(Position begin,Position end) {
-		Line line = new Line(begin, end);
-
-		//Cong: this method check whether the imaginary path intersect with boundary (cause knock into boundary event)
-		return true;
+		if (pointIsInTheRoom(begin) || pointIsInTheRoom(end)){
+			return true;
+		}else{
+			return false;
+		}
 	}
 
-	public Line returnAcrossWall(Position begin,Position end) {
 
-
-	
 	public Line returnAcrossWall(Position p,Position r) {
+		Line flyLine = new Line(p,r);
+		int i = 0;
+		for(Line boundaries : boundaries)
+		{
+			i++;
+			if(Line.doInteresct(flyLine, boundaries))
+				break;
+		}
+		return boundaries.get(i);
+	}
 
-		//return the intersected wall
-		p1 = p;
-		p3 = r;
-		return new Line(new Position(0,0),new Position(1,1));
+	public List<Line> returnWall(){
+			return this.boundaries;
 	}
-	
-	public List<Line> returnWall() {
-		return this.boundarys;
-	}
-	
-	
+
+
 }
